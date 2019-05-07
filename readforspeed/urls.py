32b3f4051fc6f78ff2876666e1b6a4282ae597db
Mike Kejser <48475633+Mikekejser@users.noreply.github.com>
2019-05-07 19:48:16 +0200

    Update urls.py

--- readforspeed/urls.py
+++ readforspeed/urls.py
@@ -6,7 +6,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
-    path('login/', auth_views.login, name='login'),
-    path('logout/', auth_views.logout, name='logout'),
+    path('LoginView/', auth_views.login, name='login'),
+    path('LogoutView/', auth_views.logout, name='logout'),
     path('oauth/', include('social_django.urls', namespace='social')),  # <--
 ]
